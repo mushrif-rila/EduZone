486a3e8d99e44c264f9fe591847cd63b1d9b53b1
mushrif-rila <mushrifrila@gmail.com>
2024-10-11 17:18:40 +0530

    Update home.jsx

--- my-app/src/DashboardApp/pages/dashboard/home.jsx
+++ my-app/src/DashboardApp/pages/dashboard/home.jsx
@@ -550,7 +550,7 @@ useEffect(() => {
       <CardBody className="pt-0">
         <div className="min-h-screen">
           <Input
-            label="Ask me"
+            label="Question me"
             icon={<button onClick={handleGPTSubmit}> <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
             </svg></button>}
